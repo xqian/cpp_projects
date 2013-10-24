f64e9094b606a195d6016d367930e5ab4ab605e2
wavelet <wavelet.liu@gmail.com>
2013-10-24 00:22:53 -0700

    sqrt

---
+++ lxb/leetcode/sqrt.cpp
@@ -0,0 +1,24 @@
+http://oj.leetcode.com/problems/sqrtx/
+
+class Solution {
+public:
+    int sqrt(int x) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (x < 2) return x;
+        
+        int low = 0;
+        int high = x;
+        int mid;
+        while(low<high){
+            //Pay attention to Integer overflow
+            mid = low + (high-low-1)/2 + 1;
+            
+            int div = x/mid;
+            if (mid == div) return mid;
+            else if (mid > div ) high = mid - 1;
+            else low = mid;
+            }
+        
+        return low;
+    }
+};
