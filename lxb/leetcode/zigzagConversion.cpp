303b5bc8804810d1bed41a246f9852b9d8736e70
wavelet <wavelet.liu@gmail.com>
2013-10-28 22:42:34 -0700

    fix zigzag

--- lxb/leetcode/zigzagConversion.cpp
+++ lxb/leetcode/zigzagConversion.cpp
@@ -1,5 +1,35 @@
 http://oj.leetcode.com/problems/zigzag-conversion/
 
+class Solution {
+public:
+    string convert(string s, int nRows) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (s.size() < 2) return s;
+        if (nRows < 2) return s;
+        
+        int zigzag_len = nRows * 2 - 2;
+        string ret = "";
+        
+        for (int i=0; i<nRows; i++){
+            for (int base = i;base < s.size() ;base += zigzag_len)
+            {
+                // first/last row
+                ret.append(1,s[base]);
+                // middle
+                if (i > 0 && i < nRows - 1) {
+                    int n = base + zigzag_len - 2*i;
+                    if (n < s.size())   ret.append(1,s[n]);
+                    else break;
+                }
+            }
+        }
+        
+        return ret;
+    }
+};
+
+// version 0: if nRows = 1,it will fail
 Note:  Memory Limit Exceeded
 
 class Solution {
