8e5aeae4a445b3a1e818871e7602930a7f2fc337
wavelet <wavelet.liu@gmail.com>
2013-10-29 23:07:56 -0700

    palindromePartitioning2.cpp

--- lxb/leetcode/palindromePartitioning2.cpp
+++ lxb/leetcode/palindromePartitioning2.cpp
@@ -1,7 +1,4 @@
-Issue: Not passed,
 http://oj.leetcode.com/problems/palindrome-partitioning-ii/
-
-Runtime Error: Last executed input:	"ab"
 
 class Solution {
 public:
@@ -11,21 +8,18 @@ public:
         
         int n = s.size();
         vector<int> DP(n+1);  //(DP[i] means:min cut for str in (i..n-1) )
-        for (int i=0; i<n; i--){
+        for (int i=0; i<=n; i++){
             DP[i] = n - i;
         }
         
         //if s[i..j] is palindrom, return true;
-        vector<bool> f(n,false);
-        vector<vector<bool> > isPal(s.size(),f);
-        for (int i=0; i<n;i++) isPal[i][i] = true;
-        
-        //for (int i=0; i<n-1;i++) if (s[i] == s[i+1]) isPal[i][i+1] = true;
+        vector<bool> oneDimension(n, false);
+        vector<vector<bool> > isPal(n, oneDimension);
         
         //DP 
         for (int j=n-1; j>=0; j--)
-        for (int k =j; k<n;k++){
-            if (s[j] == s[k] && (k-j<2 || isPal[j+1][k-1])){
+        for (int k =j; k<n; k++){
+            if (s[j] == s[k] && ((k-j)<2 || isPal[j+1][k-1])){
                 isPal[j][k] = true;
                 DP[j] = min(DP[j], 1 + DP[k+1]);
             }
