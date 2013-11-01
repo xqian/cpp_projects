83bdfcdfe8d8cc6e2bc2192bf8fc75532bc0677e
wavelet <wavelet.liu@gmail.com>
2013-11-01 11:56:57 -0700

    Update palindromePartitioning2.cpp

--- lxb/leetcode/palindromePartitioning2.cpp
+++ lxb/leetcode/palindromePartitioning2.cpp
@@ -1,5 +1,15 @@
 http://oj.leetcode.com/problems/palindrome-partitioning-ii/
 
+/*
+Question：
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+*/
+
 class Solution {
 public:
     int minCut(string s) {
