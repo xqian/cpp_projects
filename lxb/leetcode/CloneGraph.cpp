ba4fb248a959a6757433941d882acb9d2dbc611b
wavelet <wavelet.liu@gmail.com>
2013-10-31 23:39:03 -0700

    Need find bug in method 1

--- lxb/leetcode/CloneGraph.cpp
+++ lxb/leetcode/CloneGraph.cpp
@@ -1,4 +1,65 @@
 http://oj.leetcode.com/problems/clone-graph/
+
+Round2: Method 1 report wrong answer. Do you know why?
+
+Input:	{0,1,5#1,2,5#2,3#3,4,4#4,5,5#5}
+Output:	{0,1,5#1,2,5#2,3#3,4,4#4,5,5,5,5#5}
+Expected:	{0,1,5#1,2,5#2,3#3,4,4#4,5,5#5}
+
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        
+        if (!node) return NULL;
+        
+        unordered_map<UndirectedGraphNode *, UndirectedGraphNode *> mapTable;
+        set<int> visited;
+        
+        //BFS
+        queue<UndirectedGraphNode *> Q;
+        Q.push(node);
+        
+        while(!Q.empty()){
+            UndirectedGraphNode *curr = Q.front();
+            Q.pop();
+            visited.insert(curr->label);
+            
+            //clone it.
+            if (mapTable.find(curr) == mapTable.end()){
+                mapTable[curr] = new UndirectedGraphNode(curr->label);
+            }
+            
+            //put neighbour into Queue
+            for (int i=0; i<curr->neighbors.size();i++){
+                // create neighbour if not visited before.
+                if (mapTable.find(curr->neighbors[i]) == mapTable.end()){
+                    mapTable[curr->neighbors[i]] = new UndirectedGraphNode(curr->neighbors[i]->label);
+                }
+                
+                //clone neighbor relationship
+                mapTable[curr]->neighbors.push_back(mapTable[curr->neighbors[i]]);
+                
+                if (visited.find(curr->neighbors[i]->label) == visited.end()){
+                    Q.push(curr->neighbors[i]);
+                }
+            }
+        }
+        
+        return mapTable[node];
+    }
+};
+
+/* Method 2 */
+
 /**
  * Definition for undirected graph.
  * struct UndirectedGraphNode {
@@ -35,7 +96,6 @@ public:
                 UndirectedGraphNode *cloneNeighborNode;
                 
                 if (visited.find(curr->neighbors[i]->label) == visited.end()){
-                    visited[curr->neighbors[i]->label] = true;
                     Q.push(curr->neighbors[i]);
                     
                     //clone neighbors
@@ -54,4 +114,3 @@ public:
     }
 };
 
-// test:  
