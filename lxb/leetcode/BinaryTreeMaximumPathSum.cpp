af6e7388826dc2e53ff5da88768625b26a2056a7
wavelet <wavelet.liu@gmail.com>
2013-11-01 20:05:44 -0700

    add orignal question

--- lxb/leetcode/BinaryTreeMaximumPathSum.cpp
+++ lxb/leetcode/BinaryTreeMaximumPathSum.cpp
@@ -1,4 +1,22 @@
 http://oj.leetcode.com/problems/binary-tree-maximum-path-sum/
+
+/*
+Given a binary tree, find the maximum path sum.
+
+The path may start and end at any node in the tree.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+Return 6.
+
+Discuss
+
+
+*/
 /**
  * Definition for binary tree
  * struct TreeNode {
--- lxb/leetcode/Candy.cpp
+++ lxb/leetcode/Candy.cpp
@@ -1,5 +1,15 @@
 http://oj.leetcode.com/problems/candy/
 
+/*
+There are N children standing in a line. Each child is assigned a rating value.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+*/
+
 class Solution {
 public:
     int candy(vector<int> &ratings) {
