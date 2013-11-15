598e2d8a89b0760a9ca6b0901bdd95e6c16c874e
wavelet <wavelet.liu@gmail.com>
2013-11-14 22:17:10 -0800

    WordBreakII passed

--- lxb/leetcode/WordBreakII.cpp
+++ lxb/leetcode/WordBreakII.cpp
@@ -1,22 +1,16 @@
 http://oj.leetcode.com/problems/word-break-ii/
 
-/* Note: Not passed yet. There is some problem in the DP boundary part, need further check */
-
 /* Question
  Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
-
 Return all such possible sentences.
 
 For example, given
 s = "catsanddog",
 dict = ["cat", "cats", "and", "sand", "dog"].
-
 A solution is ["cats and dog", "cat sand dog"].
-
 Discuss
-
-
 */
+
 class Solution {
 public:
     vector<string> wordBreak(string s, unordered_set<string> &dict) {
@@ -24,32 +18,33 @@ public:
         vector<string> oneSolution;
         vector<string> result;
         
-        vector<bool> DP(s.size()+1, false);  //DP[i] means 0..i is breakable.
-        vector<vector<bool> > S(s.size()+1,vector<bool>(s.size(),false)); //S[i..j] whether str[i..j] is breakable
-        DP[0] = true;
+        vector<bool> DP(s.size()+1, false);  //DP[i] means 1..i is breakable.
         
-        for (int i=1; i<s.size()+1; i++)
-        for (int j=i-1;j>=0; j--)
-        {
-            if (DP[j] && dict.find(s.substr(j,i-j+1)) != dict.end()) {
-                DP[i] = true;
-                S[j][i] = true;
-                break;
-            }
-        }
+        //Can't make any
+        if (wordBreakable(s, dict,DP) == false) return result;
         
-        //0 row unused
-        for (int k=0;k<s.size();k++){
-            S[k][0] = true;
-        }
-        
-        
-        DFS(s, 0, oneSolution, result, dict, DP, S);
+        DFS(s, 0, oneSolution, result, dict, DP);
         return result;
     }
     
 private:
-    void DFS(string s, int index, vector<string> &oneSolution, vector<string> &result, unordered_set<string> &dict,vector<bool> &DP, vector<vector<bool> > &S)
+    bool wordBreakable(string s, unordered_set<string> &dict, vector<bool> &DP)
+    {
+        DP[0] = true;
+        
+        for (int i=1; i<=s.size(); ++i)
+        for (int j=0; j<i; ++j)
+        {
+            if (DP[j] && dict.find(s.substr(j,i-j)) != dict.end()) {
+                DP[i] = true;
+                break;
+            }
+        }
+        
+        return DP[s.size()];
+    }
+    
+    void DFS(string s, int index, vector<string> &oneSolution, vector<string> &result, unordered_set<string> &dict,vector<bool> &DP)
     {
         //base
         if (index == s.size()){
@@ -67,11 +62,14 @@ private:
         
         //extension
         for (int i=index ; i<s.size(); i++){
-            if (S[index][i]  == false) continue;
-            //cut branch
-            if (dict.find(s.substr(index,i-index+1)) != dict.end()){
-                oneSolution.push_back(s.substr(index,i-index+1));
-                DFS(s, i+1, oneSolution, result, dict, DP, S);
+            // DP[i] means iTH character. so mapping to i+1.
+            if (DP[i+1] == false) continue; 
+            
+            string word = s.substr(index,i-index+1);
+            if (dict.find(word) != dict.end())
+            {
+                oneSolution.push_back(word);
+                DFS(s, i+1, oneSolution, result, dict, DP);
                 //recovery
                 oneSolution.pop_back();
             }
