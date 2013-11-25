f8e67a674c5232d8e5c214acbc4d8c486f53f5a3
wavelet <wavelet.liu@gmail.com>
2013-11-25 12:45:47 -0800

    UniqueBST.cpp

--- lxb/leetcode/UniqueBST.cpp
+++ lxb/leetcode/UniqueBST.cpp
@@ -1,5 +1,21 @@
 http://oj.leetcode.com/problems/unique-binary-search-trees/
 
+/*Question:
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+
+Solution: 1. DFS.
+	  1. DP.
+
 class Solution {
 public:
     int numTrees(int n) {
@@ -17,5 +33,32 @@ public:
         }
         
         return num;
+    }
+};
+
+
+Method2: DP  
+class Solution {
+public:
+    int numTrees(int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+       
+       // DP[i]: the unique BST numbers for 1..i;
+       // DP[0] = 1;
+       // DP[i] += DP[j] * DP[i-j-1] : j=[0..i-1]
+       // return DP[n]
+
+       vector<int> DP(n+1,0); 
+       DP[0] = 1;
+
+       for (int i=1; i<=n; ++i)
+       {
+            for (int j=0; j<i; ++j)
+            {
+                DP[i] += DP[j]*DP[i-j-1];
+            }
+       }
+       
+       return DP[n];
     }
 };
