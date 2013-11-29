a0740a621f87dbfdb7422579f811e1bb1ace2182
wavelet <wavelet.liu@gmail.com>
2013-11-28 19:09:27 -0800

    WordLadderII.cpp

--- lxb/leetcode/WordLadderII.cpp
+++ lxb/leetcode/WordLadderII.cpp
@@ -18,58 +18,83 @@ Note:
 All words have the same length.
 All words contain only lowercase alphabetic characters.
 
-/* Super hard!!! How to build path in BFS? One failed try code listed here */
+Solution: 1. Super hard!!! 
+	  2. How to build path in BFS? backtrace.
+
 class Solution {
 public:
     vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
-        vector<vector<string> > res;
-        if (start.size() != end.size() || start.size() == 0) return res;
-        
-        queue<string> Q;
-        Q.push(start);
-        int level = 0;
-        
-        vector<string> solution;
-        set<string> visited; //handle duplicate
-        
-        //bfs
-        while(!Q.empty()){
-            level++;
-            int levelSize = Q.size();
-            
-            for (int i=0; i< levelSize; i++){
-                string curr = Q.front(); Q.pop();
-                solution.push_back(curr);
-                if (curr == end){
-                    //find one.
-                    res.push_back(solution);
-                }
-                
-                //handle neighbor
-                for (int i=0; i<curr.size(); i++)
-                {
-                    for (char ch='a'; ch <='z'; ch++)
-                    {
-                        if (ch == curr[i]) continue;
+       
+       unordered_set<string> visited; //duplicate detection
+       unordered_map<string, vector<string> > father; //trace 
+       unordered_set<string> current, next; //BFS, visit per level.
+       
+       bool found = false;
+       current.insert(start);
+       dict.insert(end);  // this will greatly reduce the possible comparation.
+       
+       while(!current.empty() && !found)
+       {
+           for (auto word:current) visited.insert(word);  //mark this level
+           
+           for (auto word:current){
+               for (int i=0; i<word.size(); i++){
+                    string new_word = word;
+
+                    //extend neighbor.
+                    for (char c='a';c<='z'; c++){
+                        if (c == new_word[i]) continue;
                         
-                        char tmp = curr[i];
-                        curr[i] = ch;
-                        if (dict.find(curr) != dict.end() && visited.find(curr) == visited.end()){
-                            Q.push(curr);
-                            visited.insert(curr);
+                        swap(c, new_word[i]);
+                        
+                        if (new_word == end) found = true;
+                        
+                        if ( visited.find(new_word) == visited.end() && dict.find(new_word) != dict.end())
+                        {
+                            next.insert(new_word);
+                            father[new_word].push_back(word);  //trace it!
                         }
                         
-                        curr[i] = tmp;
+                        swap(c, new_word[i]);
                     }
-                }
-                
-                //clean up
-                solution.pop_back();
+               }
+           }
+           
+           current.clear();
+           swap(current, next);
+       }
+       
+       //now build result.
+       vector<vector<string> > res;
+       
+       if (found){
+           vector<string> path;
+           buildPath(father, path, start, end, res);
+       }
+       
+       return res;
+    }
+    
+private:
+    //traceback
+    void buildPath(unordered_map<string, vector<string> > &father, vector<string> &path, const string &start, const string &word, vector<vector<string> > &res)
+    {
+        //similary to DFS.
+        path.push_back(word);
+        
+        if (word == start){
+            //find one.
+            res.push_back(path);
+            reverse(res.back().begin(),res.back().end());
+        }else{
+            for (auto f: father[word]){
+                buildPath(father, path, start, f, res);
             }
         }
         
+        //recovery
+        path.pop_back();
     }
 };
