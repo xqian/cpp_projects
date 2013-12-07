b599a1b3a2a64d1da9602c7ef9cb61742c53281d
wavelet <wavelet.liu@gmail.com>
2013-12-07 15:32:51 -0800

    BoyerMoore.cpp

---
+++ lxb/knowledge/BoyerMoore.cpp
@@ -0,0 +1,35 @@
+enum { R = 256 };
+
+//Idea: match pattern from right side to left side and skip when mismatch. Never backup. N/M normal case.
+class BoyerMoore{
+public:
+  BoyerMoore(string pat){
+	pattern = pat;
+	int M = pat.size();
+	right.resize(R, -1);  //-1 means not in current pattern.
+	for (int j=0; j<R; ++j){
+	  right[pattern[j]] = j;
+	}
+  }
+
+  int search(string text){
+    //search for pattern in text
+    int M=pattern.size();
+    int N=text.size();
+
+    int skip = 0;
+    for (int i=0;i<N - M; i+=skip){
+	for (int j=M-1; j>=0; --j){
+	   if (pattern[j] != text[i+j]){
+		//mismatch
+		skip=max(1, j-right[text[i+j]]);
+		break;
+	   }
+        }
+
+	if (skip==0) return i;
+    }
+	
+    return N;  //not found.
+  }
+}
