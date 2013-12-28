d1595985c594b77499cba4e48ea3f85ea644f795
wavelet <wavelet.liu@gmail.com>
2013-12-27 22:48:09 -0800

    rewrite tree decode

--- lxb/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
+++ lxb/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
@@ -47,3 +47,49 @@ public:
         return root;
     }
 };
+
+
+Method 2: Use array start and length to travel array to avoid one off error.
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        return helper(preorder, 0, preorder.size(), inorder, 0, inorder.size());
+    }
+    
+    TreeNode *helper(vector<int> &preorder, int pstart, int plen, vector<int> &inorder, int istart, int inlen)
+    {
+        if (plen <= 0 || inlen <= 0) return NULL;
+      
+        //get root from preorder
+        TreeNode *root = new TreeNode(preorder[pstart]);
+        
+        // find left child length
+        int llen = 0;  //left child length
+        for (int i=0; i<inlen;++i)
+        {
+            if (preorder[pstart] != inorder[i+istart]){
+                llen++;
+            }else{
+                break;
+            }
+        }
+        
+        int rlen = inlen - llen - 1;  //right child length
+        
+        root->left = helper(preorder, pstart + 1,llen, inorder, istart, llen);
+        root->right = helper(preorder, pstart + llen + 1, rlen, inorder, istart + llen + 1, rlen);
+        
+        return root;
+    }
+};
